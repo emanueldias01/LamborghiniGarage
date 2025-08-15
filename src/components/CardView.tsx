dd70b6677bc9705e6b919d62d728a6326741f7cf
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2025-08-15 16:35:04 -0300

    add image from api

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -1,10 +1,19 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import logo from '../../assets/logo.png'
+import { CAR_ASSETS_BASE_URL } from "../consts/car";
 
 export default function CardView(){
     return(
         <View style={styles.containerCard}>
             <Image style={styles.image} source={logo}/>
+            <View style={styles.car}>
+                <Text style={styles.carBrand}>Lamborghini</Text>
+                <Text style={styles.carName}>Model</Text>
+                <Image
+                style={styles.imageCar} 
+                source={{uri: `${CAR_ASSETS_BASE_URL}1.png`}}
+                />
+            </View>
         </View>
     )
 }
@@ -25,8 +34,30 @@ const styles = StyleSheet.create({
         resizeMode: 'contain',
         width: '65%',
         height: '30%',
-        marginBottom: 200,
+        marginBottom: 20,
         borderBottomWidth: 2,
         borderBottomColor: '#01A6B3'
+    },
+    car: {
+        alignItems: 'center',
+    },
+    carBrand : {
+        color: '#fff',
+        fontWeight: '400',
+        fontSize: 18,
+        fontStyle : 'italic'
+    },
+    carName: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+
+    },
+    imageCar: {
+        resizeMode: 'contain',
+        alignItems: 'center',
+        width: 300,
+        height: 300,
+        marginBottom: 10
     }
 })
